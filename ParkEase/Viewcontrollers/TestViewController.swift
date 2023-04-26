89d4e33a0fce2b6399b4f1d7a457088d4cf83bb7
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-04-26 17:33:24 +0500

    Added TestViewController

---
+++ ParkEase/Viewcontrollers/TestViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TestViewController.swift
+//  ParkEase
+//
+//  Created by Dmitry Polyakov on 26.04.2023.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+    let test = "Test"
+}
