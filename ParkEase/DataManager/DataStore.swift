bfe12ddde0a8f92d13036475f2939d5c30e2bbc7
Sergey Polyakov <PolyusIO@yandex.ru>
2023-05-14 16:02:58 +0500

    Added an empty DataStore and grouped ViewControllers

---
+++ ParkEase/DataManager/DataStore.swift
@@ -0,0 +1,14 @@
+//
+//  DataStore.swift
+//  ParkEase
+//
+//  Created by Сергей Поляков on 14.05.2023.
+//
+
+class DataStore {
+    static let shared = DataStore()
+    
+    // Add cars here
+    
+    private init() {}
+}
