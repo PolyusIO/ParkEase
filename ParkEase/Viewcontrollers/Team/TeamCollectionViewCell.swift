d9bef51765dfcc42ffdfb2a7af35bcaa6a807532
Sergey Polyakov <PolyusIO@yandex.ru>
2023-05-14 15:45:27 +0500

    Add Team ViewController and Storyboard

---
+++ ParkEase/Viewcontrollers/Team/TeamCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TeamCollectionViewCell.swift
+//  ParkEase
+//
+//  Created by Сергей Поляков on 14.05.2023.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ParkEase/Viewcontrollers/Team/TeamViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TeamViewController.swift
+//  ParkEase
+//
+//  Created by Сергей Поляков on 14.05.2023.
+//
+
+import UIKit
+
+class TeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
