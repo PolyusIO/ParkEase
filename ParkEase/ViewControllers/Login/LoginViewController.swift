d2daaa03a44eb3c881509d6e44b61d4fa0c6e0a3
artemyderik <derry0808@gmail.com>
2023-05-16 22:12:24 +0700

    LoginVC works. Entering logic was made - simple login/password check.

--- ParkEase/ViewControllers/Login/LoginViewController.swift
+++ ParkEase/ViewControllers/Login/LoginViewController.swift
@@ -8,11 +8,55 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet var loginButton: UIButton!
     
+    //MARK: Private properties
+    private let user = User.getUserData()
+    
+    //MARK: IB Outlets
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var enterButton: UIButton!
+    
+    //MARK: Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    override  func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    //MARK: IB Actions
+    @IBAction func enterButtonTapped() {
+        guard loginTextField.text == user.login, passwordTextField.text == user.password else {
+            showAlert(
+                with: "ОЙ!",
+                and: "Неправильный логин или пароль. Попробуйте еще раз.",
+                textField: passwordTextField
+            )
+            return
+        }
+    }
+}
 
+//MARK: Extension
+extension LoginViewController {
+    private func showAlert(
+        with title: String,
+        and message: String,
+        textField: UITextField
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAlert = UIAlertAction(title: "OK", style: .default) { _ in
+            textField.text = ""
+        }
+        
+        present(alert, animated: true)
+        alert.addAction(okAlert)
     }
 }
